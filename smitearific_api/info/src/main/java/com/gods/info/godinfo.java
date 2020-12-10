7f4a408516e92e5830decb3ccc84c76167ef8999
i403318 <realsnowkid@gmail.com>
2020-12-11 00:21:42 +0200

    Updated godInfo API to get data from a HiRezAPI file

--- smitearific_api/info/src/main/java/com/gods/info/godinfo.java
+++ smitearific_api/info/src/main/java/com/gods/info/godinfo.java
@@ -17,18 +17,41 @@ public class godinfo {
     private String type;
     private String role;
     private String lane;
-    private String ability1;
-    private String ability2;
-    private String ability3;
-    private String ability4;
-    private String ability5;
+    private String icon;
+    private String card;
+    private String ability1Name;
+    private String ability2Name;
+    private String ability3Name;
+    private String ability4Name;
+    private String ability5Name;
     private String ability1URL;
     private String ability2URL;
     private String ability3URL;
     private String ability4URL;
     private String ability5URL;
-    private String icon;
-    private String card;
+    private String ability1Description;
+    private String ability2Description;
+    private String ability3Description;
+    private String ability4Description;
+    private String ability5Description;
+    private String ability1Cooldown;
+    private String ability2Cooldown;
+    private String ability3Cooldown;
+    private String ability4Cooldown;
+    private String ability1Cost;
+    private String ability2Cost;
+    private String ability3Cost;
+    private String ability4Cost;
+    private String ability1Menuitems;
+    private String ability2Menuitems;
+    private String ability3Menuitems;
+    private String ability4Menuitems;
+    private String ability5Menuitems;
+    private String ability1Rankitems;
+    private String ability2Rankitems;
+    private String ability3Rankitems;
+    private String ability4Rankitems;
+    private String ability5Rankitems;
 
     public godinfo() {
     }
@@ -89,44 +112,60 @@ public class godinfo {
         this.lane = lane;
     }
 
-    public String getAbility1() {
-        return ability1;
+    public String getIcon() {
+        return icon;
     }
 
-    public void setAbility1(String ability) {
-        this.ability1 = ability;
+    public void setIcon(String icon) {
+        this.icon = icon;
     }
 
-    public String getAbility2() {
-        return ability2;
+    public String getCard() {
+        return card;
     }
 
-    public void setAbility2(String ability) {
-        this.ability2 = ability;
+    public void setCard(String card) {
+        this.card = card;
     }
 
-    public String getAbility3() {
-        return ability3;
+    public String getAbility1Name() {
+        return ability1Name;
     }
 
-    public void setAbility3(String ability) {
-        this.ability3 = ability;
+    public void setAbility1Name(String ability) {
+        this.ability1Name = ability;
     }
 
-    public String getAbility4() {
-        return ability4;
+    public String getAbility2Name() {
+        return ability2Name;
     }
 
-    public void setAbility4(String ability) {
-        this.ability4 = ability;
+    public void setAbility2Name(String ability) {
+        this.ability2Name = ability;
     }
 
-    public String getAbility5() {
-        return ability5;
+    public String getAbility3Name() {
+        return ability3Name;
     }
 
-    public void setAbility5(String ability) {
-        this.ability5 = ability;
+    public void setAbility3Name(String ability) {
+        this.ability3Name = ability;
+    }
+
+    public String getAbility4Name() {
+        return ability4Name;
+    }
+
+    public void setAbility4Name(String ability) {
+        this.ability4Name = ability;
+    }
+
+    public String getAbility5Name() {
+        return ability5Name;
+    }
+
+    public void setAbility5Name(String ability) {
+        this.ability5Name = ability;
     }
 
     public String getAbility1URL() {
@@ -169,20 +208,187 @@ public class godinfo {
         this.ability5URL = abilityURL;
     }
 
-    public String getIcon() {
-        return icon;
+    public String getAbility1Description() {
+        return ability1Description;
     }
 
-    public void setIcon(String icon) {
-        this.icon = icon;
+    public void setAbility1Description(String abilityDescription) {
+        this.ability1Description = abilityDescription;
     }
 
-    public String getCard() {
-        return card;
+    public String getAbility2Description() {
+        return ability2Description;
     }
 
-    public void setCard(String card) {
-        this.card = card;
+    public void setAbility2Description(String abilityDescription) {
+        this.ability2Description = abilityDescription;
     }
 
+    public String getAbility3Description() {
+        return ability3Description;
+    }
+
+    public void setAbility3Description(String abilityDescription) {
+        this.ability3Description = abilityDescription;
+    }
+
+    public String getAbility4Description() {
+        return ability4Description;
+    }
+
+    public void setAbility4Description(String abilityDescription) {
+        this.ability4Description = abilityDescription;
+    }
+
+    public String getAbility5Description() {
+        return ability5Description;
+    }
+
+    public void setAbility5Description(String abilityDescription) {
+        this.ability5Description = abilityDescription;
+    }
+
+    public String getAbility1Cooldown() {
+        return ability1Cooldown;
+    }
+
+    public void setAbility1Cooldown(String abilityCooldown) {
+        this.ability1Cooldown = abilityCooldown;
+    }
+
+    public String getAbility2Cooldown() {
+        return ability2Cooldown;
+    }
+
+    public void setAbility2Cooldown(String abilityCooldown) {
+        this.ability2Cooldown = abilityCooldown;
+    }
+
+    public String getAbility3Cooldown() {
+        return ability3Cooldown;
+    }
+
+    public void setAbility3Cooldown(String abilityCooldown) {
+        this.ability3Cooldown = abilityCooldown;
+    }
+
+    public String getAbility4Cooldown() {
+        return ability4Cooldown;
+    }
+
+    public void setAbility4Cooldown(String abilityCooldown) {
+        this.ability4Cooldown = abilityCooldown;
+    }
+
+    public String getAbility1Cost() {
+        return ability1Cost;
+    }
+
+    public void setAbility1Cost(String abilityCost) {
+        this.ability1Cost = abilityCost;
+    }
+
+    public String getAbility2Cost() {
+        return ability2Cost;
+    }
+
+    public void setAbility2Cost(String abilityCost) {
+        this.ability2Cost = abilityCost;
+    }
+
+    public String getAbility3Cost() {
+        return ability3Cost;
+    }
+
+    public void setAbility3Cost(String abilityCost) {
+        this.ability3Cost = abilityCost;
+    }
+
+    public String getAbility4Cost() {
+        return ability4Cost;
+    }
+
+    public void setAbility4Cost(String abilityCost) {
+        this.ability4Cost = abilityCost;
+    }
+
+    public String getAbility1Menuitems() {
+        return ability1Menuitems;
+    }
+
+    public void setAbility1Menuitems(String abilityMenuitems) {
+        this.ability1Menuitems = abilityMenuitems;
+    }
+
+    public String getAbility2Menuitems() {
+        return ability2Menuitems;
+    }
+
+    public void setAbility2Menuitems(String abilityMenuitems) {
+        this.ability2Menuitems = abilityMenuitems;
+    }
+
+    public String getAbility3Menuitems() {
+        return ability3Menuitems;
+    }
+
+    public void setAbility3Menuitems(String abilityMenuitems) {
+        this.ability3Menuitems = abilityMenuitems;
+    }
+
+    public String getAbility4Menuitems() {
+        return ability4Menuitems;
+    }
+
+    public void setAbility4Menuitems(String abilityMenuitems) {
+        this.ability4Menuitems = abilityMenuitems;
+    }
+
+    public String getAbility5Menuitems() {
+        return ability5Menuitems;
+    }
+
+    public void setAbility5Menuitems(String abilityMenuitems) {
+        this.ability5Menuitems = abilityMenuitems;
+    }
+
+    public String getAbility1Rankitems() {
+        return ability1Rankitems;
+    }
+
+    public void setAbility1Rankitems(String abilityRankitems) {
+        this.ability1Rankitems = abilityRankitems;
+    }
+
+    public String getAbility2Rankitems() {
+        return ability2Rankitems;
+    }
+
+    public void setAbility2Rankitems(String abilityRankitems) {
+        this.ability2Rankitems = abilityRankitems;
+    }
+
+    public String getAbility3Rankitems() {
+        return ability3Rankitems;
+    }
+
+    public void setAbility3Rankitems(String abilityRankitems) {
+        this.ability3Rankitems = abilityRankitems;
+    }
+
+    public String getAbility4Rankitems() {
+        return ability4Rankitems;
+    }
+
+    public void setAbility4Rankitems(String abilityRankitems) {
+        this.ability4Rankitems = abilityRankitems;
+    }
+
+    public String getAbility5Rankitems() {
+        return ability5Rankitems;
+    }
+
+    public void setAbility5Rankitems(String abilityRankitems) {
+        this.ability5Rankitems = abilityRankitems;
+    }
 }
--- smitearific_api/info/src/main/java/com/gods/info/test.java
+++ smitearific_api/info/src/main/java/com/gods/info/test.java
@@ -1,30 +1,187 @@
 package com.gods.info;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.util.StringUtils;
-
-import net.minidev.json.JSONObject;
-import net.minidev.json.parser.JSONParser;
-import net.minidev.json.parser.ParseException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class test {
 
-    public static void main(String[] args) {
-        JSONParser jpar = new JSONParser(JSONParser.MODE_PERMISSIVE);
-        try {
-            JSONObject jo = (JSONObject) jpar.parse(new FileReader(
-                    "E:/Repositories/smitearific/smitearific_api/info/src/main/java/com/gods/info/test.json"));
-            String str = jo.toString();
-            StringUtils.replace(str, "\\", " ");
-            System.out.println(str);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            throw new IllegalArgumentException(e);
+    static void InsertGods(InputStream is) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        String str = restTemplate.getForObject("http://localhost:8081/godinfoes", String.class);
+        JSONObject jObject = new JSONObject(str);
+        JSONArray jArray = jObject.getJSONObject("_embedded").getJSONArray("godinfoes");
+
+        if (jArray.length() >= 111) {
+            System.out.println("Gods Already Exist");
+            DeleteGods(is);
+            PostGods(is);
+        } else {
+            System.out.println("God are being posted");
+            PostGods(is);
         }
+
+    }
+
+    static void PostGods(InputStream is) {
+        JSONTokener tokener = new JSONTokener(is);
+        JSONArray ja = new JSONArray(tokener);
+        System.out.println(ja.length());
+        for (int i = 0; i < ja.length(); i++) {
+            JSONObject jo = ja.getJSONObject(i);
+            JSONObject jobj = new JSONObject();
+            jobj.put("name", jo.getString("Name").replaceAll("\\s", ""));
+            jobj.put("title", jo.getString("Title"));
+            jobj.put("pantheon", jo.getString("Pantheon"));
+            jobj.put("type", jo.getString("Type"));
+            jobj.put("role", jo.getString("Roles"));
+            jobj.put("icon", jo.getString("godIcon_URL"));
+            jobj.put("card", jo.getString("godCard_URL"));
+            jobj.put("ability1Name", jo.getJSONObject("Ability_1").getString("Summary"));
+            jobj.put("ability1URL", jo.getJSONObject("Ability_1").getString("URL"));
+            jobj.put("ability1Description", jo.getJSONObject("Ability_1").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("description"));
+            jobj.put("ability1Cooldown", StringUtils.replace(jo.getJSONObject("Ability_1").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("cooldown"), "s", " seconds"));
+            jobj.put("ability1Cost", jo.getJSONObject("Ability_1").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("cost") + " mana");
+            jobj.put("ability1Menuitems", jo.getJSONObject("Ability_1").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("menuitems").toString());
+            jobj.put("ability1Rankitems", jo.getJSONObject("Ability_1").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("rankitems").toString());
+            jobj.put("ability2Name", jo.getJSONObject("Ability_2").getString("Summary"));
+            jobj.put("ability2URL", jo.getJSONObject("Ability_2").getString("URL"));
+            jobj.put("ability2Description", jo.getJSONObject("Ability_2").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("description"));
+            jobj.put("ability2Cooldown", StringUtils.replace(jo.getJSONObject("Ability_2").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("cooldown"), "s", " seconds"));
+            jobj.put("ability2Cost", jo.getJSONObject("Ability_2").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("cost") + " mana");
+            jobj.put("ability2Menuitems", jo.getJSONObject("Ability_2").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("menuitems").toString());
+            jobj.put("ability2Rankitems", jo.getJSONObject("Ability_2").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("rankitems").toString());
+            jobj.put("ability3Name", jo.getJSONObject("Ability_3").getString("Summary"));
+            jobj.put("ability3URL", jo.getJSONObject("Ability_3").getString("URL"));
+            jobj.put("ability3Description", jo.getJSONObject("Ability_3").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("description"));
+            jobj.put("ability3Cooldown", StringUtils.replace(jo.getJSONObject("Ability_3").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("cooldown"), "s", " seconds"));
+            jobj.put("ability3Cost", jo.getJSONObject("Ability_3").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("cost") + " mana");
+            jobj.put("ability3Menuitems", jo.getJSONObject("Ability_3").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("menuitems").toString());
+            jobj.put("ability3Rankitems", jo.getJSONObject("Ability_3").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("rankitems").toString());
+            jobj.put("ability4Name", jo.getJSONObject("Ability_4").getString("Summary"));
+            jobj.put("ability4URL", jo.getJSONObject("Ability_4").getString("URL"));
+            jobj.put("ability4Description", jo.getJSONObject("Ability_4").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("description"));
+            jobj.put("ability4Cooldown", StringUtils.replace(jo.getJSONObject("Ability_4").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("cooldown"), "s", " seconds"));
+            jobj.put("ability4Cost", jo.getJSONObject("Ability_4").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("cost") + " mana");
+            jobj.put("ability4Menuitems", jo.getJSONObject("Ability_4").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("menuitems").toString());
+            jobj.put("ability4Rankitems", jo.getJSONObject("Ability_4").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("rankitems").toString());
+            jobj.put("ability5Name", jo.getJSONObject("Ability_5").getString("Summary"));
+            jobj.put("ability5URL", jo.getJSONObject("Ability_5").getString("URL"));
+            jobj.put("ability5Description", jo.getJSONObject("Ability_5").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getString("description"));
+            jobj.put("ability5Menuitems", jo.getJSONObject("Ability_5").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("menuitems").toString());
+            jobj.put("ability5Rankitems", jo.getJSONObject("Ability_5").getJSONObject("Description")
+                    .getJSONObject("itemDescription").getJSONArray("rankitems").toString());
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<String> request = new HttpEntity<String>(jobj.toString(), headers);
+
+            godinfo godinfo = restTemplate.postForObject("http://localhost:8081/godinfoes", request, godinfo.class);
+
+            System.out.println(godinfo.getName());
+        }
+    }
+
+    static void DeleteGods(InputStream is) {
+        JSONTokener tokener = new JSONTokener(is);
+        JSONArray ja = new JSONArray(tokener);
+        RestTemplate restTemplate = new RestTemplate();
+        for (int i = 0; i < ja.length(); i++) {
+
+            int p = i + 1;
+
+            restTemplate.delete("http://localhost:8081/godinfoes/" + p);
+
+            System.out.println("Deleted @ http://localhost:8081/godinfoes/" + p);
+
+        }
+
+        String str = null;
+        try {
+            str = restTemplate.getForObject("http://localhost:8081/godinfoes/search/resetAutoIncrement", String.class);
+        } catch (HttpClientErrorException ex) {
+            if (ex.getStatusCode() != HttpStatus.NOT_FOUND) {
+                throw ex;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String resourceName = "/data.json";
+        InputStream inputStream = test.class.getResourceAsStream(resourceName);
+        if (inputStream == null) {
+            throw new NullPointerException("Cannot find resource file " + resourceName);
+        }
+
+        InsertGods(inputStream);
+
+        // try {
+        // File myObj = new File(
+        // "E:/Repositories/smitearific/smitearific_api/info/src/main/resources/properData2.json");
+        // if (myObj.createNewFile()) {
+        // System.out.println("File created: " + myObj.getName());
+        // FileWriter myWriter = new FileWriter(
+        // "E:/Repositories/smitearific/smitearific_api/info/src/main/resources/properData2.json");
+        // myWriter.write(jo.toString());
+        // myWriter.close();
+        // System.out.println("Successfully wrote to the file.");
+        // } else if (myObj.delete()) {
+        // System.out.println("Deleted the file: " + myObj.getName());
+        // if (myObj.createNewFile()) {
+        // System.out.println("File created: " + myObj.getName());
+        // FileWriter myWriter = new FileWriter(
+        // "E:/Repositories/smitearific/smitearific_api/info/src/main/resources/properData2.json");
+        // myWriter.write(jo.toString());
+        // myWriter.close();
+        // System.out.println("Successfully wrote to the file.");
+        // } else {
+        // System.out.println("Failed to create or delete the file.");
+        // }
+        // } else {
+        // System.out.println("Failed to create or delete the file.");
+        // }
+        // } catch (IOException e) {
+        // System.out.println("An error occurred.");
+        // e.printStackTrace();
+        // }
     }
 }
